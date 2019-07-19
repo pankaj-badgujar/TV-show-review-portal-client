7784b2a072423f083ff9532ebfebbb3143b488db
Sujana <nethi.s@husky.neu.edu>
2019-07-18 21:56:46 -0400

    package json update

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(__dirname + '/dist/wbdv-project-sf19-angular-client-group13'));
+app.listen(process.env.PORT || 3000);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/wbdv-project-sf19-angular-client-group13/index.html'));
+})
+console.log('console listening');
